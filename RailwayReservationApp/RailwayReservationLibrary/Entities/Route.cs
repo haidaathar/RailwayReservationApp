62d63692e9c7eeef70f28bc27f991bc413a0dfe3
haidaathar <hadiaathar2005>
2024-04-17 10:36:51 -0700

    Models Created By Hadia

---
+++ RailwayReservationApp/RailwayReservationLibrary/Entities/Route.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayReservationLibrary.Entities
+{
+    public class Route
+    {
+        public int Id { get; set; }
+        public string To { get; set; }
+        public string From { get; set; }
+        public int FixedFare { get; set; }
+        public int TrainId { get; set; }
+    }
+}
---
+++ RailwayReservationApp/RailwayReservationLibrary/Entities/Trains.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayReservationLibrary.Entities
+{
+    public class Trains
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DepartureFrom { get; set; }
+        public string Destination { get; set; }
+        public Datetime DepartureTime { get; set; }
+        public Datetime ArrivalTime { get; set; }
+    }
+}
